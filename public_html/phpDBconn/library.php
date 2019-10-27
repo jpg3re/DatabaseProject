a13279e64a2ab3fa98509d7e9b575bfd3073c4ee
isaac <itr9f@virginia.edu>
2019-10-27 14:34:11 -0400

    skeleton code

---
+++ public_html/phpDBconn/library.php
@@ -0,0 +1,7 @@
+<?php
+	$SERVER = 'http://cs4750.cs.virginia.edu';
+	$USERNAME = 'yourComputingID';
+	$PASSWORD = 'yourPassword';
+	$DATABASE = 'yourComputingID';
+?>
+
---
+++ public_html/testphp.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>Introduction to PHP</title></head>
+<body>
+
+  <?php
+	echo "Hi, I'm a PHP script!<BR><BR>";
+	$i = 0; 
+	echo "The variable \"i\" is " . $i;
+
+	$x = 2;
+	$y = 3;
+	$z = $x + $y;
+	echo "<BR><BR>";
+	echo $x . " plus " . $y . " is: " . $z; 
+  ?>
+
+</body>
+</html>
+
