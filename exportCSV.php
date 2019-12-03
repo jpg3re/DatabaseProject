7f463f6e061a1ed98917b81fcd5ea865518bcb64
elr8cu <elr8cu@virginia.edu>
2019-12-02 23:54:41 -0500

    fixed exportCSV.php

--- exportCSV.php
+++ exportCSV.php
@@ -10,69 +10,49 @@ mysqli_connect_error());
      if(isset($_SESSION["username"])){
          $username=$_SESSION["username"];
          $sql="SELECT uid FROM user WHERE username='$username'";
-         $result = mysqli_query($con,$sql);
+         $result=mysqli_query($con,$sql);
          if(mysqli_num_rows($result)>0){
-            echo "first table worked";
             $row= $result->fetch_row();
             $uid=$row[0];
             $sql="SELECT gid FROM participates WHERE uid='$uid' ";
-            $result = mysqli_query($con,$ql);
+            $result=mysqli_query($con,$sql);
             if(mysqli_num_rows($result)>0){
                 $row= $result->fetch_row();
                 $gid=$row[0];
                 $sql="SELECT sid FROM queue WHERE gid='$gid' ORDER BY position ASC ";
-                $result = mysqli_query($con,$sql);
+                $result=mysqli_query($con,$sql);
                 if(mysqli_num_rows($result)>0){
-                    echo "next table worked";
-                    $delimiter = ",";
-                    $filename = 'queue.csv';
-                    $f = fopen($filename, 'w');
-                //$csv_data = '';
-                    $fields = array('#', 'Song', 'Artist', 'Mood');
-                //for ($i = 0; $i < 4; $i++) {
-                //    $csv_data.= $fields[$i].',';
-                //}
-                //$csv_data.= '
-    		//';
-                    fputcsv($f, $fields, $delimiter);
-                    echo "about to write!";
-                    while ($row = $result->fetch_row()) {
+                    $filename='queue.csv';
+                    $csv_data='';
+                    $fields=array('Position in Queue', 'Song', 'Artist', 'Mood');
+                    for ($i = 0; $i < 4; $i++) {
+                       $csv_data.= $fields[$i].',';
+                    }
+                    $csv_data.='
+    		    ';
+                    $position=0;
+                    while ($row=$result->fetch_row()) {
+                        $position++;
                         $songid=$row[0];
-                        $sql2 = "SELECT * FROM song WHERE sid ='$songid' ";
+                        $sql2="SELECT * FROM song WHERE sid ='$songid' ";
                         $result2=mysqli_query($con,$sql2);
                         $row2=$result2->fetch_row();
-                        $data = array($row2[0], $row2[1], $row2[2], $row2[3]);
-                        fputcsv($f, $data, $delimiter);
-                    //for ($i = 0; $i < 4; $i++) {
-                    //    $csv_data.= $data[$i].',';
-                    //}
-		    //$csv_data.= '
-                    //';
+                        $data=array($position, $row2[1], $row2[2], $row2[3]);
+                        for ($i = 0; $i < 4; $i++) {
+                           $data_remove_end=rtrim($data[$i], ',');
+                           $curr_data=str_replace(',',' and',$data_remove_end);
+                           $csv_data.=$curr_data.',';
+                        }
+		        $csv_data.='
+                        ';
                     }
-                #fseek($f, 0);
-                #fclose($f);
-                #header('Content-Description: File Transfer');
-                #header('Content-Type: application/csv; ');
-                #header('Content-Disposition: attachment; filename='.$filename);
-                #fpassthru($f);
-                #readfile($filename);
-                #header('Content-Encoding: UTF-8');
-                #header('Content-type: text/csv; charset=UTF-8');
-                #header("Content-Disposition: attachment; filename=".$filename."");
-                #echo "\xEF\xBB\xBF";
-                #echo($csv_data);
-                    fclose($f);
-                    header("Content-Description: File Transfer");
-                    header("Content-Disposition: attachment; filename=".$filename);
-                    header("Content-Type: application/csv; "); 
-                    readfile($filename);
-                    unlink($filename);
-                    exit();
+                    header('Content-Encoding: UTF-8');
+                    header('Content-type: text/csv; charset=UTF-8');
+                    header("Content-Disposition: attachment; filename=".$filename."");
+                    echo($csv_data);
                 }
             }
         }
      }
-     echo "successfully downloaded";
-     //header('Location: myList.php');
      mysqli_close($con);
 ?>
